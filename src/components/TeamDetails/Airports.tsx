ca6493236e167ed39066e2d8c5ad7a33f51bf5d4
agul72 <alexandr.gul@gmail.com>
2023-11-05 01:21:03 +0200

    Ads link text style changed

--- src/components/TeamDetails/Airports.tsx
+++ src/components/TeamDetails/Airports.tsx
@@ -3,21 +3,21 @@ import { iAirport } from "../../services/teams/iTeam";
 import s from "./team.module.scss";
 
 interface Props {
-    airports: iAirport[],
+  airports: iAirport[],
 }
 
 function Airports({ airports }: Props): JSX.Element {
-    return (
-        <div >
-            <div className={s.aidaTitle}><h3>Airports:</h3></div>
-            {airports.map((airport: iAirport, airIndex: number) =>
-                <div key={airIndex} className={s.aidaItem}>
-                    <div><strong>{airport.name}</strong></div>
-                    <div><a href={airport.link}>Go to airport site</a></div>
-                    <div>{airport.details}</div>
-                </div>)
-            }
+  return (
+    <div >
+      <div className={s.aidaTitle}><h3>Airports:</h3></div>
+      {airports.map((airport: iAirport, airIndex: number) =>
+        <div key={airIndex} className={s.aidaItem}>
+          <div><strong>{airport.name}</strong></div>
+          <div><a href={airport.link} target="_blank">Go to airport site</a></div>
+          <div>{airport.details}</div>
         </div>)
+      }
+    </div>)
 }
 
 export default Airports;
--- src/components/TeamDetails/Highlights.tsx
+++ src/components/TeamDetails/Highlights.tsx
@@ -3,29 +3,29 @@ import { iInterestHighlight } from "../../services/teams/iTeam";
 import s from "./team.module.scss";
 
 interface Props {
-    highlights: iInterestHighlight[],
+  highlights: iInterestHighlight[],
 }
 
 function Highlights({ highlights }: Props): JSX.Element {
-    return (
-        <div>
-            <div><h3>Highlights:</h3></div>
-            {highlights.map((highlight: iInterestHighlight, index: number) =>
-                <div key={index}>
-                    <div className={s.place}>
-                        <div className={s.plaseImageWrapper}>
-                            <img src={'/assets/images/places/' + highlight.image}
-                            alt="Photo" className={s.image} />
-                        </div>
-                        <div>
-                            <div><strong>{highlight.name}</strong></div>
-                            <div><a href={highlight.link}>Go to {highlight.name} site</a></div>
-                            <div>{highlight.description}</div>
-                        </div>
-                    </div>
-                </div>)
-            }
+  return (
+    <div>
+      <div><h3>Highlights:</h3></div>
+      {highlights.map((highlight: iInterestHighlight, index: number) =>
+        <div key={index}>
+          <div className={s.place}>
+            <div className={s.plaseImageWrapper}>
+              <img src={'/assets/images/places/' + highlight.image}
+                alt="Photo" className={s.image} />
+            </div>
+            <div>
+              <div><strong>{highlight.name}</strong></div>
+              <div><a href={highlight.link} target="_blank">Go to {highlight.name} site</a></div>
+              <div>{highlight.description}</div>
+            </div>
+          </div>
         </div>)
+      }
+    </div>)
 }
 
 export default Highlights;
